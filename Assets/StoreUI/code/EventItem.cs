2d854aaa4f25752dcad231167689006eb58c0b93
kzsvps <kzsvps950428@gmail.com>
2025-06-30 03:19:20 +0800

    完成商家資訊卡功能後端串接

--- Assets/StoreUI/code/EventItem.cs
+++ Assets/StoreUI/code/EventItem.cs
@@ -1,12 +1,45 @@
+using System;
+
 [System.Serializable]
 public class EventItem
 {
+    public int id;
     public string title;
     public string distance;
     public string introduce;
-    public string city; //縣市
+    public string city;
+    public string phone;
+    public string address;
+    public string type;
+}
 
-    public string phone;     // ☑️ 電話
-    public string address;   // ☑️ 地址
-    public string type;      // ☑️ 類別（Type）
+[System.Serializable]
+public class ShopListResponse
+{
+    public ShopData[] shops;
+    public int currentPage;
+    public int totalPages;
+    public long totalElements;
+    public bool hasNext;
+}
+
+[System.Serializable]
+public class ShopData
+{
+    public int id;
+    public string title;
+    public string distance;
+    public string introduce;
+    public string city;
+    public string phone;
+    public string address;
+    public string type;
+}
+
+[System.Serializable]
+public class FilterOptions
+{
+    public string[] cities;
+    public string[] types;
+    public string[] distances;
 }
